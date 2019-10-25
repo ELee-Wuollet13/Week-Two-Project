7e21e98c8fa62c650a14aa0597d49d08f40900c5
Everett Lee-Wuollet <everett.leewuollet@gmail.com>
2019-10-25 09:56:50 -0700

    adding to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $("form#intake").submit(function(event) {
     let num1 = parseInt($("input:radio[name=color]:checked").val());
-    let num2 = parseInt($("input:radio[name=color]:checked").val());
-    let num3 = parseInt($("input:radio[name=color]:checked").val());
-    let num4 = parseInt($("input:radio[name=color]:checked").val());
-    let num5 = parseInt($("input:radio[name=color]:checked").val());
+    let num2 = parseInt($("input:radio[name=hand]:checked").val());
+    let num3 = parseInt($("input:radio[name=pet]:checked").val());
+    let num4 = parseInt($("input:radio[name=drink]:checked").val());
+    let num5 = parseInt($("input:radio[name=career]:checked").val());
     let numTotal = (num1 + num2 + num3 + num4 + num5)
 
     if (numTotal => 2) {
@@ -14,11 +14,8 @@ $(document).ready(function() {
     } else {
       $('.result').show(c#)
     }
-    console.log(num1)
-    console.log(num2)
-    console.log(num3)
-    console.log(num4)
-    console.log(num5)
+    console.log(numTotal)
+
     event.preventDefault();
   });
 });
