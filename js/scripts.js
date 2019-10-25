370a777440f2eade6ee78e2b76e75d3cb4a930ee
Everett Lee-Wuollet <everett.leewuollet@gmail.com>
2019-10-25 16:46:36 -0700

    edit for ease of reading

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Front End Logic
 $(document).ready(function() {
   $("form#intake").submit(function() {
     $("results").hide(".javascript .ruby .c");
@@ -7,23 +8,17 @@ $(document).ready(function() {
     let num4 = parseInt($("input:radio[name=drink]:checked").val());
     let num5 = parseInt($("input:radio[name=career]:checked").val());
     let num = (num1 += num2 += num3 += num4 += num5);
-
+//Back End Logic
     if (isNaN(num)) {
       alert("Please fill out all fields")
-    }
-    else if (num > 2) {
-      console.log("hi")
+    } else if (num > 2) {
       $('.result').html("javascript!")
     } else if (num < 2 && num > -2) {
-      console.log("mid")
       $('.result').html("Ruby!")
-
     } else {
-      console.log("low")
       $('.result').html("C#!")
     }
     console.log(num)
-
     event.preventDefault();
   });
 });
